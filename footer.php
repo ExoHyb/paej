64a47db9c5fdb4652f3c29cdbe22b9ec31ecaf50
ExoHyb <theo.martin22@icloud.com>
2016-04-21 14:47:33 +0200

    Création de la maquette pour le site du PAEJ ; Requiert SASS & Bower

---
+++ footer.php
@@ -0,0 +1,11 @@
+        <footer>
+            <p>
+                Copyright (c) 2016 Copyright Holder All Rights Reserved.
+            </p>
+        </footer>
+        <!-- Call Scripts -->
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <!-- End call Scripts -->
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Point Accueil Ecoute Jeunes</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
+        <link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="container">
+                	<div class="row">
+                		<div class="mod-header">
+                			<div class="col-md-5">
+                			<div class="menu-general menu-gauche text-right">
+                					<ul>
+                						<li><a class="element-menu-1" href="#">Accueil</a></li>
+                						<li><a class="element-menu-2" href="#">Actions/Activités</a></li>
+                						<li><a class="element-menu-3" href="#">Actualités</a></li>
+                					</ul>
+                				</div>
+                			</div>
+                			<div class="col-md-2">
+                				<img src="img/logo.png" alt="logo paej">
+                			</div>
+                			<div class="col-md-5">
+                				<div class="menu-general menu-droite">
+                					<ul>
+                						<li><a class="element-menu-4" href="#">Cartes</a></li>
+                						<li><a class="element-menu-5" href="#">Questions/FAQ</a></li>
+                						<li><a class="element-menu-6" href="dashboard/dashboard.php">Gestion site</a></li>
+                					</ul>
+                				</div>
+                			</div>
+                		</div>
+                	</div>
+                </div>
+            </nav>
+        </header>
