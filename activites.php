72c1644cf7b3433b22e27ec04048d6e63af61f4c
Nadège Jahier <jahiernadege@yahoo.fr>
2016-05-04 09:14:33 +0200

    modif page activites

---
+++ activites.php
@@ -0,0 +1,86 @@
+<?php include "header.php" ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="row">
+        <div class="col-md-12">
+
+      <div class="action">
+      <h2>Action 1:</h2><p> 12/02/2016, PAEJ Mende, 14h</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      </p>
+    </div>
+  </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+
+    <div class="action">
+    <h2>Action 2:</h2><p> 12/02/2016, PAEJ Mende, 14h</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+  </div>
+</div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+
+  <div class="action">
+  <h2>Action 3:</h2><p> 12/02/2016, PAEJ Mende, 14h</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+</div>
+</div>
+</div>
+</div>
+
+<div class="row">
+  <div class="col-md-6">
+    <div class="row">
+      <div class="col-md-12">
+
+    <div class="activite">
+    <h2>Activité 1:</h2><p> 12/02/2016, PAEJ Mende, 14h</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+  </div>
+</div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+
+  <div class="activite">
+  <h2>Activité 2:</h2><p> 12/02/2016, PAEJ Mende, 14h</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+</div>
+</div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+
+<div class="activite">
+<h2>Activité 3:</h2><p> 12/02/2016, PAEJ Mende, 14h</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+</div>
+<?php include "footer.php" ?>
