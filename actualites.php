81f43e58e88a0bdb96f6ffa9b369af90259d218b
Nadège Jahier <jahiernadege@yahoo.fr>
2016-05-04 14:24:04 +0200

    debut page actualites, modif activites

--- actualites.php
+++ actualites.php
@@ -1,3 +1,92 @@
 <?php include "header.php" ?>
+<div class="container">
+  <!--Ouverture row englobante-->
+    <div class="row">
+        <div class="col-md-12">
+          <div class="deco-actualites">
+            <div class="row">
+              <h1> Actualités en Lozère</h1>
+              <div class="col-md-3">
+                <img class="img-responsive" src="http://placehold.it/250x250" alt="" />
+              </div>
+            <div class="col-md-9">
+            <div class="background-title">
+              <h2>Concert Lorem ipsum dolor sit amet</h2>
+            </div>
+            <div class="separate-line"></div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod...
+              <a href="#">Lire la suite</a>
+              </p>
+            </div>
+        </div>
+        </div>
+
+        <div class="deco-actualites">
+            <div class="row">
+              <div class="col-md-3">
+                <img class="img-responsive" src="http://placehold.it/250x250" alt="" />
+              </div>
+            <div class="col-md-9">
+            <div class="background-title">
+              <h2>Lorem ipsum dolor sit amet</h2>
+            </div>
+            <div class="separate-line"></div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod...
+              <a href="#">Lire la suite</a>
+              </p>
+            </div>
+        </div>
+        </div>
+
+      <div class="deco-actualites">
+        <div class="row">
+          <h1>Actualités Nationales</h1>
+          <div class="col-md-3">
+            <img class="img-responsive" src="http://placehold.it/250x250" alt="" />
+          </div>
+        <div class="col-md-9">
+        <div class="background-title">
+          <h2>Concert ipsum dolor sit amet</h2>
+        </div>
+        <div class="separate-line"></div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod...
+          <a href="#">Lire la suite</a>
+          </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="deco-actualites">
+      <div class="row">
+        <div class="col-md-3">
+          <img class="img-responsive" src="http://placehold.it/250x250" alt="" />
+        </div>
+      <div class="col-md-9">
+      <div class="background-title">
+        <h2>Lorem ipsum dolor sit amet</h2>
+      </div>
+      <div class="separate-line"></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incidi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod...
+        <a href="#">Lire la suite</a>
+        </p>
+      </div>
+  </div>
+  </div>
+</div>
+</div><!--Fermeture row englobante-->
+</div>
+
 
 <?php include "footer.php" ?>
