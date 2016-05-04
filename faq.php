2cdae425e10ba0bf75afb9e7e8be74b4225bca4b
Nadège Jahier <jahiernadege@yahoo.fr>
2016-05-04 16:05:54 +0200

    debut page faq

---
+++ faq.php
@@ -0,0 +1,35 @@
+<?php include "header.php" ?>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="deco-faq">
+        <div class="background-title2">
+          <h2>Les Questions Fréquentes</h2>
+        </div>
+        <div class="separate-line2"></div>
+        <ul>
+          <li><a href="#"><i class="fa fa-arrow-right" aria-hidden="true"></i>
+            Question ? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.
+          </a></li>
+          <li><a href="#"><i class="fa fa-arrow-right" aria-hidden="true"></i>
+            Question ? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.
+          </a></li>
+          <li><a href="#"><i class="fa fa-arrow-right" aria-hidden="true"></i>
+            Question ? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.
+          </a></li>
+          <li><a href="#"><i class="fa fa-arrow-right" aria-hidden="true"></i>
+            Question ? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.
+          </a></li>
+          <li><a href="#"><i class="fa fa-arrow-right" aria-hidden="true"></i>
+            Question ? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.
+          </a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+</div>
+<?php include "footer.php" ?>
